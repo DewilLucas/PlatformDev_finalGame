616540b99d9ba0b321641a5cab17538c78661d6c
Lucas Dewil <lucas.dewil@student.howest.be>
2023-04-20 16:51:31 +0200

    BaseMovement

---
+++ Sackboy/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.Translate(direction * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
