e693515210aa9b186853f455aaa766eaf7539d20
Lucas Dewil <lucas.dewil@student.howest.be>
2023-04-20 23:45:41 +0200

    Added animation of movement

--- Sackboy/Assets/Scripts/CharacterMovement.cs
+++ Sackboy/Assets/Scripts/CharacterMovement.cs
@@ -8,7 +8,12 @@ public class CharacterMovement : MonoBehaviour
     public float jumpForce = 1.0f;
     private bool isGrounded = false;
     private Rigidbody rb;
-    private int DoubleJump = 0;
+
+    private int doubleJumpCount = 0;
+    public int maxDoubleJumps = 1;
+
+    
+    public Animator anim;
 
     void Start()
     {
@@ -26,31 +31,27 @@ public class CharacterMovement : MonoBehaviour
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
             transform.Translate(direction * speed * Time.deltaTime, Space.World);
+            anim.SetBool("IsMoving", true);
+        }
+        else
+        {
+            anim.SetBool("IsMoving", false);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true )
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            DoubleJump++;
-            if (DoubleJump <= 2)
-            {
-                Debug.Log(DoubleJump);
-                isGrounded = true;
-                if (DoubleJump == 1)
-                {
-                    rb.AddForce(Vector3.up * (jumpForce * 1f), ForceMode.Impulse);
-                }
 
-                if (DoubleJump > 1)
-                {
-                    rb.AddForce(Vector3.up * (jumpForce * 0.5f), ForceMode.Impulse);
-                }
-            }
-            else
+            if (isGrounded)
             {
+                anim.SetBool("IsJumping", true);
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
                 isGrounded = false;
-                DoubleJump = 0;
             }
-            
+            else if (doubleJumpCount < maxDoubleJumps)
+            {
+                rb.AddForce(Vector3.up * (jumpForce / 2), ForceMode.Impulse);
+                doubleJumpCount++;
+            }
         }
     }
 
@@ -59,6 +60,8 @@ public class CharacterMovement : MonoBehaviour
         if (collision.gameObject.layer == 3) // IF he's touching the ground
         {
             isGrounded = true;
+            anim.SetBool("IsJumping", !isGrounded);
+            doubleJumpCount = 0;
         }
     }
 }
