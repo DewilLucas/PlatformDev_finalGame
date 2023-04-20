c4fa6602b47894d0b87ef6dea8aca4e9e3df01fa
Lucas Dewil <lucas.dewil@student.howest.be>
2023-04-20 17:48:32 +0200

    Jump start

--- Sackboy/Assets/Scripts/CharacterMovement.cs
+++ Sackboy/Assets/Scripts/CharacterMovement.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float jumpForce = 1.0f;
+    private bool isGrounded = false;
+    private Rigidbody rb;
+    private int DoubleJump = 0;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
@@ -18,6 +26,39 @@ public class CharacterMovement : MonoBehaviour
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
             transform.Translate(direction * speed * Time.deltaTime, Space.World);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true )
+        {
+            DoubleJump++;
+            if (DoubleJump <= 2)
+            {
+                Debug.Log(DoubleJump);
+                isGrounded = true;
+                if (DoubleJump == 1)
+                {
+                    rb.AddForce(Vector3.up * (jumpForce * 1f), ForceMode.Impulse);
+                }
+
+                if (DoubleJump > 1)
+                {
+                    rb.AddForce(Vector3.up * (jumpForce * 0.5f), ForceMode.Impulse);
+                }
+            }
+            else
+            {
+                isGrounded = false;
+                DoubleJump = 0;
+            }
+            
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 3) // IF he's touching the ground
+        {
+            isGrounded = true;
         }
     }
 }
