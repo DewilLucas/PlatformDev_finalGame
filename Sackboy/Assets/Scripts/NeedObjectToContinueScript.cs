92351085d013d21909a23457756e756bf0307af2
Lucas Dewil <lucas.dewil@student.howest.be>
2023-05-04 12:43:07 +0200

    Door open with trigger

--- Sackboy/Assets/Scripts/NeedObjectToContinueScript.cs
+++ Sackboy/Assets/Scripts/NeedObjectToContinueScript.cs
@@ -5,13 +5,71 @@ using UnityEngine;
 public class NeedObjectToContinueScript : MonoBehaviour
 {
     public GameObject requiredObject;
-    // Start is called before the first frame update
+
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+
+    public float pivotSpeed = 50.0f; // The speed at which the door pivots
+    public float pivotAngle = 80.0f; // The angle to which the door pivots
+
+    private bool isOpening = false;
+    private bool isClosing = false;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == requiredObject)
         {
             Debug.Log("Object placed on trigger pad!");
-            // Perform any desired actions here...
+            isOpening = true;
+            isClosing = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == requiredObject)
+        {
+            Debug.Log("Object removed from trigger pad!");
+            isOpening = false;
+            isClosing = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isOpening)
+        {
+            float leftDoorTargetRotation = -pivotAngle;
+            float rightDoorTargetRotation = pivotAngle;
+
+            // Smoothly pivot the doors to the desired rotation
+            float leftDoorRotation = Mathf.MoveTowardsAngle(leftDoor.transform.localEulerAngles.y,
+                leftDoorTargetRotation, pivotSpeed * Time.deltaTime);
+            float rightDoorRotation = Mathf.MoveTowardsAngle(rightDoor.transform.localEulerAngles.y,
+                rightDoorTargetRotation, pivotSpeed * Time.deltaTime);
+
+            // Set the new door rotations
+            leftDoor.transform.localEulerAngles = new Vector3(leftDoor.transform.localEulerAngles.x, leftDoorRotation,
+                leftDoor.transform.localEulerAngles.z);
+            rightDoor.transform.localEulerAngles = new Vector3(rightDoor.transform.localEulerAngles.x,
+                rightDoorRotation, rightDoor.transform.localEulerAngles.z);
+        }
+        else if (isClosing)
+        {
+            float leftDoorTargetRotation = 0.0f;
+            float rightDoorTargetRotation = 0.0f;
+
+            // Smoothly pivot the doors to the desired rotation
+            float leftDoorRotation = Mathf.MoveTowardsAngle(leftDoor.transform.localEulerAngles.y,
+                leftDoorTargetRotation, pivotSpeed * Time.deltaTime);
+            float rightDoorRotation = Mathf.MoveTowardsAngle(rightDoor.transform.localEulerAngles.y,
+                rightDoorTargetRotation, pivotSpeed * Time.deltaTime);
+
+            // Set the new door rotations
+            leftDoor.transform.localEulerAngles = new Vector3(leftDoor.transform.localEulerAngles.x, leftDoorRotation,
+                leftDoor.transform.localEulerAngles.z);
+            rightDoor.transform.localEulerAngles = new Vector3(rightDoor.transform.localEulerAngles.x,
+                rightDoorRotation, rightDoor.transform.localEulerAngles.z);
         }
     }
 }
