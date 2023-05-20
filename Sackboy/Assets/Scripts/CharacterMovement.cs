16bdc076d1a395d0a5460fc702cef84ba59b198e
Lucas Dewil <lucas.dewil@student.howest.be>
2023-05-20 15:14:23 +0200

    Starrt pauze scrript

--- Sackboy/Assets/Scripts/CharacterMovement.cs
+++ Sackboy/Assets/Scripts/CharacterMovement.cs
@@ -40,127 +40,131 @@ public class CharacterMovement : MonoBehaviour
     private bool _isDead = false;
     private int deadAnimBlock = 0;
     public GameObject[] Lives;
-
+    public MainGameScript mainGameScript;
     void Start()
     {
         controller = GetComponent<CharacterController>();
     }
     void Update()
     {
-
-        if (_isDead && deadAnimBlock == 1)
+        if (!mainGameScript.isPaused)
         {
-            if (!anim.GetCurrentAnimatorStateInfo(0).IsName("IsDead"))
+            if (_isDead && deadAnimBlock == 1)
             {
-                anim.Play("IsDead");
-            }
-            else if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
-            {
-                anim.speed = 0f; // Pause the animation at the end
-            }
-            return;
-        }
-        if (Ennemies != null)
-        {
-            foreach (var ennemy in Ennemies)
-            {
-                if (ennemy.hasHitTarget)
+                if (!anim.GetCurrentAnimatorStateInfo(0).IsName("IsDead"))
                 {
-                    if (Input.GetKeyDown(KeyCode.V))
+                    anim.Play("IsDead");
+                }
+                else if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+                {
+                    anim.speed = 0f; // Pause the animation at the end
+                }
+                return;
+            }
+            if (Ennemies != null)
+            {
+                foreach (var ennemy in Ennemies)
+                {
+                    if (ennemy.hasHitTarget)
                     {
-                        ennemy.hasHitTarget = false;
-                        ennemy.gameObject.SetActive(false); // kill the ennemy
-                    }
-                    else
-                    {
-                        _EnnemyHitCounter++;
-                        if (_EnnemyHitCounter == 50)
+                        if (Input.GetKeyDown(KeyCode.V))
                         {
-                            _EnnemyHitCounter = 0;
-                            _lives--;
-                            Lives[_lives].SetActive(false);
-                            if (_lives <= 0)
-                            {
-                                _lives = 0;
-                                _isDead = true;
-                                anim.SetBool("IsDead", true);
-                                deadAnimBlock++;
-                            }
+                            ennemy.hasHitTarget = false;
+                            ennemy.gameObject.SetActive(false); // kill the ennemy
                         }
-                        Debug.Log(_lives);
+                        else
+                        {
+                            _EnnemyHitCounter++;
+                            if (_EnnemyHitCounter == 50)
+                            {
+                                _EnnemyHitCounter = 0;
+                                _lives--;
+                                Lives[_lives].SetActive(false);
+                                if (_lives <= 0)
+                                {
+                                    _lives = 0;
+                                    _isDead = true;
+                                    anim.SetBool("IsDead", true);
+                                    deadAnimBlock++;
+                                }
+                            }
+                            Debug.Log(_lives);
+                        }
+
                     }
-                    
                 }
             }
-        }
-        
 
-        JumpPadTimer += Time.deltaTime;
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-       // moveDirection = new Vector3(horizontal, 0, vertical).normalized;
-        // Only update the x and z components of moveDirection if the player is not jumping
-        if (!isJumping)
-        {
-            moveDirection = new Vector3(horizontal, 0, vertical).normalized;
-        }
-        else
-        {
-            // Keep the x and z components of moveDirection, and only update the y component
-            //moveDirection += Physics.gravity * Time.deltaTime;
 
-            controller.Move(Vector3.down * Time.deltaTime * 9.81f);
-            if (controller.isGrounded ||Math.Floor(moveDirection.y) <= 0)
+            JumpPadTimer += Time.deltaTime;
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            // moveDirection = new Vector3(horizontal, 0, vertical).normalized;
+            // Only update the x and z components of moveDirection if the player is not jumping
+            if (!isJumping)
             {
-                isJumping = false;
-            }
-        }
-        if (moveDirection.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
-            Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
-            //controller.Move(moveDirection * speed * Time.deltaTime);
-            if (GrabScript.grabScript.isGrabbing == true)
-            {
-                anim.SetBool("IsGrabbing", true);
-                anim.SetBool("IsMoving", false);
+                moveDirection = new Vector3(horizontal, 0, vertical).normalized;
             }
             else
             {
-                anim.SetBool("IsGrabbing", false);
-                anim.SetBool("IsMoving", true);
+                // Keep the x and z components of moveDirection, and only update the y component
+                //moveDirection += Physics.gravity * Time.deltaTime;
+
+                controller.Move(Vector3.down * Time.deltaTime * 9.81f);
+                if (controller.isGrounded || Math.Floor(moveDirection.y) <= 0)
+                {
+                    isJumping = false;
+                }
             }
-
-        }
-        else
-        {
-            anim.SetBool("IsMoving", false);
-        }
-
-        // Check for jump pads
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 0.01f);
-        foreach (Collider collider in colliders)
-        {
-            
-            if (collider.gameObject.CompareTag("JumpPad") && controller.isGrounded && IsJumpPadEnabled == true)
+            if (moveDirection.magnitude >= 0.1f)
             {
-                JumpCharacterScript.DubbleJump = 2; // don't make the character jump again until they've landed
-                moveDirection.y = jumpVelocity;
-                // Apply the jump velocity
-                isJumping = true;
-                IsJumpPadEnabled = false;
-                JumpPadTimer = 0.0f;
+                float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+                Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+                //controller.Move(moveDirection * speed * Time.deltaTime);
+                if (GrabScript.grabScript.isGrabbing == true)
+                {
+                    anim.SetBool("IsGrabbing", true);
+                    anim.SetBool("IsMoving", false);
+                }
+                else
+                {
+                    anim.SetBool("IsGrabbing", false);
+                    anim.SetBool("IsMoving", true);
+                }
+
             }
-            
+            else
+            {
+                anim.SetBool("IsMoving", false);
+            }
+
+            // Check for jump pads
+            Collider[] colliders = Physics.OverlapSphere(transform.position, 0.01f);
+            foreach (Collider collider in colliders)
+            {
+
+                if (collider.gameObject.CompareTag("JumpPad") && controller.isGrounded && IsJumpPadEnabled == true)
+                {
+                    JumpCharacterScript.DubbleJump = 2; // don't make the character jump again until they've landed
+                    moveDirection.y = jumpVelocity;
+                    // Apply the jump velocity
+                    isJumping = true;
+                    IsJumpPadEnabled = false;
+                    JumpPadTimer = 0.0f;
+                }
+
+            }
+            // If the jump pad is disabled, wait 5 seconds before enabling it again, this is to prevent the player from jumping multiple times in a row
+            if (IsJumpPadEnabled == false && Math.Floor(JumpPadTimer) == 5)
+            {
+                JumpPadTimer = 0.0f;
+                IsJumpPadEnabled = true;
+            }
+            controller.Move(moveDirection * speed * Time.deltaTime);
         }
-        // If the jump pad is disabled, wait 5 seconds before enabling it again, this is to prevent the player from jumping multiple times in a row
-        if (IsJumpPadEnabled == false && Math.Floor(JumpPadTimer) == 5 )
-        {
-            JumpPadTimer = 0.0f;
-            IsJumpPadEnabled = true;
-        }
-        controller.Move(moveDirection * speed * Time.deltaTime);
+
+        
 
        
     }
---
+++ Sackboy/Assets/Scripts/MainGameScript.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainGameScript : MonoBehaviour
+{
+    public bool isPaused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 1f;
+            isPaused = false;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            isPaused = true;
+        }
+    }
+}
